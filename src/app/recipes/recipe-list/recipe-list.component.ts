2de559e20e1e2e981dccfea9eb5c2e394c8a0994
Sandeep <sm671903@gmail.com>
2021-11-20 23:59:10 +0530

    populating recipe-list-component.html with hard code data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   // We will store the instances of Recipe in this array which is of type Recipe....present in recipes/recipe.model.ts
-  recipes : Recipe[] = []
+  recipes : Recipe[] = [
+    new Recipe("A tet Recipe","This is first Recipe test", "https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHJlY2lwZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80")
+  ]
 
   constructor() { }
 
