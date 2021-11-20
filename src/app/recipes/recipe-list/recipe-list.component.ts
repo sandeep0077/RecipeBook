c2bc8dd286ba7f84af7b1fec4112c7663a15dbfe
Sandeep <sm671903@gmail.com>
2021-11-20 23:37:00 +0530

    editing the recipes array and commenting the message above it

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = []
+  // We will store the instances of Recipe in this array which is of type Recipe....present in recipes/recipe.model.ts
+  recipes : Recipe[] = []
 
   constructor() { }
 
